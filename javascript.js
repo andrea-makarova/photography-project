958ed906dae34508c85f9b1ecd10483b880604d4
andrea-makarova <andreamakarova99@gmail.com>
2019-02-21 20:38:08 +0100

    static content on subpages, buttons not dynamic at all

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
 const template = document.querySelector("template").content;
 let main1 = document.querySelector("main#portrait");
-let main2 = document.querySelector("section#macro");
+
+let main2 = document.querySelector("main#macro");
+
 let main3 = document.querySelector("main#wildLife");
+
 let main4 = document.querySelector("main#landscape");
+
 const productlistLink = "https://spreadsheets.google.com/feeds/list/1piIN2__3Ry5ywIrluwMNSXQYyrN36hGrG_yJI_hF-qQ/od6/public/values?alt=json";
 const modal = document.querySelector(".modal-background");
 const close = document.querySelector(".modal-button");
@@ -15,6 +19,7 @@ fetch(productlistLink).then(e => e.json()).then(data => data.feed.entry.forEach(
 function showProduct(product) {
     console.log(product)
     let clone = template.cloneNode(true);
+
     clone.querySelector("h2.name").textContent = product.gsx$name.$t;
     clone.querySelector("h3.brand").textContent = product.gsx$brand.$t;
     clone.querySelector("h3.price").textContent = product.gsx$price.$t;
@@ -27,23 +32,18 @@ function showProduct(product) {
         fetch(productlistLink).then(e => e.json()).then(data => data.feed.entry.forEach(showDetails));
     });
 
-    //    if (product.gsx$category.$t == "portrait") {
-    //        main1.appendChild(clone);
-    //    }
-    //    if (product.gsx$category.$t == "macro") {
-    //        main2.appendChild(clone);
-    //    }
-    //    if (product.gsx$category.$t == "wildlife") {
-    //        main3.appendChild(clone);
-    //    }
-    //    if (product.gsx$category.$t == "landscape") {
-    //        main4.appendChild(clone);
-    //    }
     if (product.gsx$category.$t == "portrait") {
-        main1.appendChild(clone)
+        main1.appendChild(clone);
     }
-
-
+    if (product.gsx$category.$t == "macro") {
+        main2.appendChild(clone);
+    }
+    if (product.gsx$category.$t == "wildlife") {
+        main3.appendChild(clone);
+    }
+    if (product.gsx$category.$t == "landscape") {
+        main4.appendChild(clone);
+    }
 
 };
 
@@ -56,6 +56,7 @@ function showDetails(data) {
     modal.querySelector(" p.modal-lensType").textContent = data.gsx$lenstype.$t;
     modal.querySelector(" p.modal-chip").textContent = data.gsx$chip.$t;
     modal.querySelector(" p.modal-bayonet").textContent = data.gsx$bayonet.$t;
+       modal.querySelector(" p.modal-weight").textContent = data.gsx$weight.$t;
     modal.querySelector(" p.modal-longDescription").textContent = data.gsx$longdescription.$t;
     modal.querySelector(" h3.modal-price").textContent = data.gsx$price.$t;
 
